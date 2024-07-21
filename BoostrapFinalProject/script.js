abf403155451502c3fe2159d64e2274000390274
aniket1153 <joshianiket1153@gmail.com>
2024-07-21 15:53:41 +0530

    added home page

---
+++ BoostrapFinalProject/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
+});
